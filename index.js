78e082c3dada78a8b268e0ac7eac36d8d6706486
Vadim Rotaru <zizinaki@Vadims-MacBook-Pro.local>
2021-04-11 15:57:09 +0300

    Functional program

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-
+//Save an integer between 1 and 3
 const randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log(randomNumber)
 
+/*
+1.Add a click event listener;
+2.Store the id number in clickedButton var;
+3.If the clickedButton id equal with the randomNumber change the button color in green;
+4.Otherwise, change the button color in red;
+*/
 document.addEventListener("click", function(event) {
-    console.log(Number(event.srcElement.id))
-    const clickedButton = Number(event.srcElement.id);
+    const clickedButton = Number(event.target.id);
     if (randomNumber === clickedButton) {
-        document.getElementById(randomNumber).style.backgroundColor = "green";
-        console.log("Ai ghicit")
-    } else if (clickedButton === 4) {
-        window.location.reload();
+        document.getElementById(clickedButton).style.backgroundColor = "green";
     }
-    else {
+    //clickedButton>0 to avoid the case when the click is outside the buttons;
+    else if (randomNumber !== clickedButton && clickedButton > 0) {
         document.getElementById(clickedButton).style.backgroundColor = "red";
-        console.log("mai incearca") 
     }
-    
 })
 
+//Refresh the page after 1s
+setTimeout(function(){ 
+    window.location.reload();; 
+}, 1000);
